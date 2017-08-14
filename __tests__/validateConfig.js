ae0d64029ed1c470bfe77d673a6cdd4b9d77573b
NeZeD [Mac Air] <nezed@ya.ru>
2017-08-14 18:40:33 +0300

    Fix dependecies and add infrastructure scripts

---
+++ __tests__/validateConfig.js
@@ -0,0 +1,21 @@
+const babel = require('babel-core')
+const config = require('../')
+
+describe('Validate config:', () => {
+  const transformOpts = {
+    presets: './index.js'
+  }
+
+  test('should run `babel-code` without errors', () => {
+    expect(
+      () => babel.transform(`// Empty code snippet`, transformOpts)
+    ).not.toThrowError()
+  })
+  test('should transpile .jsx', () => {
+    const { code } = babel.transform(`<div />`, transformOpts)
+
+    expect(code).toBe(
+      'React.createElement("div", null);'
+    )
+  })
+})
